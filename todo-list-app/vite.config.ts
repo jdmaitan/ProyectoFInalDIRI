6e057c9f6a6fae96d6b8829d6498ab8e67252bcb
Jose Maitan <jdmaitan@gmail.com>
2025-05-11 23:05:09 +0200

    Build para deploy

--- todo-list-app/vite.config.ts
+++ todo-list-app/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/ProyectoFInalDIRI/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react(), tailwindcss()],
 })
